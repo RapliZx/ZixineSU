4ac1a81b3102218172d3bb2098a15c0b5cf9d7c8
zixine <waheid005@gmail.com>
2026-02-19 18:47:05 +0700

    Update manager_sign.h

--- kernel/manager_sign.h
+++ kernel/manager_sign.h
@@ -53,6 +53,10 @@
 #define EXPECTED_HASH_MAMBO \
     "a9462b8b98ea1ca7901b0cbdcebfaa35f0aa95e51b01d66e6b6d2c81b97746d8"
 
+// ZixineSu/KernelSU
+#define EXPECTED_SIZE_ZIXINE 0xa8e397
+#define EXPECTED_HASH_ZIXINE \
+    "ccec15172719e47b707058f8e437be9a5d5bc778165fdcb80435f2d429ef029a"
 typedef struct {
     u32 size;
     const char *sha256;
