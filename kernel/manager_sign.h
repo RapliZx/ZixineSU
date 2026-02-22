88dd7c48fca078be214d04ad0f492d40f06bc03d
zixine <waheid005@gmail.com>
2026-02-22 22:47:03 +0700

    Update manager_sign.h

--- kernel/manager_sign.h
+++ kernel/manager_sign.h
@@ -64,6 +64,16 @@
 #define EXPECTED_HASH_ZIXINE \
     "ccec15172719e47b707058f8e437be9a5d5bc778165fdcb80435f2d429ef029a"
 
+// VorteXSU/VorteXSU
+#define EXPECTED_SIZE_VORTEXSU 0x381
+#define EXPECTED_HASH_VORTEXSU                                                    \
+    "67eec44718428adad14e6a9dca57822759aba7e77a8cad7071f6f6704df8bb48"
+
+// Dynamic Sign
+#define EXPECTED_SIZE_OTHER 0x300
+#define EXPECTED_HASH_OTHER                                                    \
+    "0000000000000000000000000000000000000000000000000000000000000000"
+
 typedef struct {
     u32 size;
     const char *sha256;
